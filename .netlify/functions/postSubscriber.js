67b5d8a6dc999c32d517a4be86c6dd067bfabb89
Michel Milmore <m.milmore2701@gmail.com>
2022-08-02 15:53:12 -0400

    refactoring app.js, postSubscriber.js

--- .netlify/functions/postSubscriber.js
+++ .netlify/functions/postSubscriber.js
@@ -21,14 +21,32 @@ exports.handler = async (event, context) => {
 
   try {
     const response = await axios.post(targetUrl, body, { headers });
+    console.log("response = ", JSON.stringify(response.response.data.message));
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Success!" }),
+      body: JSON.stringify(response.response.data.message),
     };
   } catch (error) {
+    if (error.request) {
+      console.log("error in request");
+      return {
+        statusCode: 406,
+        body: JSON.stringify({ message: "Request Not Accepted!" }),
+      };
+    }
+    if (error.response) {
+      console.log("error in response");
+      return {
+        statusCode: error.response.status,
+        body: JSON.stringify(error.response.data.message),
+      };
+    }
+    console.log("error in server or Convertkit");
     return {
-      statusCode: 400,
-      body: JSON.stringify({ message: "Error!" }),
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Internal Server Error - Try again after a short period.",
+      }),
     };
   }
 };
---
+++ src/components/AddSubscriber.jsx
@@ -0,0 +1,141 @@
+import React, { useState, useEffect } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import axios from "axios";
+
+const INIT_INFO = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  phone: "",
+};
+
+function AddSubscriber() {
+  const [info, setInfo] = useState({
+    firstName: "fn",
+    lastName: "ln",
+    email: "a@a.com",
+    phone: "514-386-2701",
+  });
+
+  const [submitBtnDisabled, setSubmitBtnDisabled] = useState(true);
+
+  useEffect(() => {
+    const { firstName, lastName, email, phone } = info;
+    setSubmitBtnDisabled(!(firstName && lastName && email && phone));
+  }, [info]);
+
+  const handleChange = ({ target: { name, value } }) => {
+    setInfo({ ...info, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const smallestArea = 201;
+    const highestArea = 989;
+    const stripPhone = info.phone.replace(/\D/g, "");
+
+    if (stripPhone.length !== 10) {
+      alert("Invalid phone number. Missing number(s) or too may numbers.");
+    } else if (
+      parseInt(stripPhone.substring(0, 3)) < smallestArea ||
+      parseInt(stripPhone.substring(0, 3)) > highestArea
+    ) {
+      alert(
+        `Phone area code must be between ${smallestArea} and ${highestArea} inclusively`
+      );
+    } else {
+      const body = info;
+      const headers = {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST",
+      };
+
+      try {
+        const response = await axios.post(
+          "/.netlify/functions/postSubscriber",
+          body,
+          { headers }
+        );
+        setInfo(INIT_INFO);
+        alert(response.data.message);
+      } catch (error) {
+        alert(error.response.data.message);
+      }
+    }
+  };
+
+  const { firstName, lastName, email, phone } = info;
+
+  return (
+    <div className="center-display">
+      <p className="h3 ms-4 mt-3">Add Subscriber Form</p>
+      <Form onSubmit={handleSubmit} className="text-start border py-2 px-4">
+        <Form.Group className="mb-3 fs-5">
+          <Form.Label className="fs-6">First name</Form.Label>
+          <Form.Control
+            onChange={handleChange}
+            type="text"
+            placeholder="Enter first name"
+            value={firstName}
+            name="firstName"
+            maxLength="20"
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3 fs-5">
+          <Form.Label className="fs-6">Last name</Form.Label>
+          <Form.Control
+            onChange={handleChange}
+            type="text"
+            placeholder="Enter last name"
+            value={lastName}
+            name="lastName"
+            maxLength="20"
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3 fs-5">
+          <Form.Label className="fs-6">Email address</Form.Label>
+          <Form.Control
+            onChange={handleChange}
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            name="email"
+          />
+          <Form.Text className="text-muted fs-6">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3 fs-5">
+          <Form.Label className="fs-6">Phone number</Form.Label>
+          <Form.Control
+            onChange={handleChange}
+            type="text"
+            placeholder="XXX-XXX-XXXX"
+            value={phone}
+            name="phone"
+            minLength="10"
+            maxLength="12"
+          />
+        </Form.Group>
+
+        <div className="text-center">
+          <Button
+            className="fs-6"
+            variant="primary"
+            type="submit"
+            disabled={submitBtnDisabled}
+          >
+            Submit
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+}
+
+export default AddSubscriber;
